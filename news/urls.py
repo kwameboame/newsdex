c2ac5c684ccbac23e00337870ed43d2dff2c5fb6
ilov3 <iloveudead@gmail.com>
2016-03-30 22:25:55 +0300

    * pep8 refactorings (indentations and new lines) * new_feed view logic changed: db savings moved to celery task * celery task parse refactored into separate tasks * Tracked_words moved to Words model (tracked boolean field) * nltk_all view now process tracked_words relations * tracked_words template displays related articles, posts and comments

--- news/urls.py
+++ news/urls.py
@@ -1,15 +1,14 @@
 from django.conf.urls import url
 from . import views
 
-
 urlpatterns = [
     url(r'^$', views.articles_list, name='articles_list'),
     url(r'^feeds/new', views.new_feed, name='feed_new'),
     url(r'^feeds/', views.feeds_list, name='feeds_list'),
     url(r'^ajax_articles', views.ajax_articles, name='ajax_articles'),
     url(r'^parse', views.parse_manual),
-    url(r'^nltk', views.nltk_all),
+    url(r'^nltk', views.nltk_all, name='nltk'),
     url(r'^trends', views.nltk_all, name='trends'),
     # url(r'^for_date', views.nltk_for_date), 
-    url(r'^for_range', views.nltk_for_range), 
+    url(r'^for_range', views.nltk_for_range),
 ]
--- newsproject/celery.py
+++ newsproject/celery.py
@@ -3,18 +3,17 @@ import django
 from datetime import timedelta
 from celery import Celery
 from django.conf import settings
- 
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'newsproject.settings')
 django.setup()
- 
+
 app = Celery('news')
- 
+
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
-
 from celery.schedules import crontab
- 
+
 CELERYBEAT_SCHEDULER = "djcelery.schedulers.DatabaseScheduler"
 # CELERYBEAT_SCHEDULE = {
 #     "get_articles": {
